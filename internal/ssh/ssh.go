ece36fbb8f353c0ef7d10e17c6bd6f9f9116aa9b
Chris Kastorff <encryptio@gmail.com>
2016-01-19 20:27:13 +0000

    internal/ssh: rename stuff, client options are now in an SSHOptions struct

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -5,66 +5,76 @@ import (
 	"os/exec"
 )
 
-// A Target is a remote location where commands can be run through SSH and
+// A Client is a remote location where commands can be run through SSH and
 // directories/files copied via rsync.
-type Target struct {
-	Hostname   string
-	Username   string
+type Client struct {
+	opts SSHOptions
+}
+
+type SSHOptions struct {
+	Host       string
+	User       string
 	KnownHosts *KnownHosts
 }
 
-// New constructs a new Target pointing at the given host and username. If
+// New constructs a new Client pointing at the given host and username. If
 // KnownHosts is non-nil, the ssh command used will use it as a known_hosts
 // file.
-func New(host, user string, knownhosts *KnownHosts) *Target {
-	return &Target{
-		Hostname:   host,
-		Username:   user,
-		KnownHosts: knownhosts,
-	}
+func New(opts SSHOptions) *Client {
+	return &Client{opts: opts}
 }
 
 // RunCommand runs the given command as a shell command on the remote host.
-func (t *Target) RunCommand(cmd string) error {
-	opts := t.sshOpts()
-	opts = append(opts, t.Hostname, cmd)
-	return runExecPassthrough(exec.Command("ssh", opts...))
+func (c *Client) RunCommand(cmd string) error {
+	args := c.sshArgs()
+	args = append(args, c.opts.Host, cmd)
+	return runPassthrough(exec.Command("ssh", args...))
 }
 
 // RsyncTo runs rsync to copy from the given local source to the given remote
 // destination.
-func (t *Target) RsyncTo(src, dst string) error {
-	c := exec.Command("rsync", "-avP", "-e", t.sshInvocation(), src, t.Hostname+":"+dst)
-	return runExecPassthrough(c)
+func (c *Client) RsyncTo(src, dst string) error {
+	cmd := exec.Command(
+		"rsync",
+		"-avP",
+		"-e", c.sshInvocation(),
+		src,
+		c.opts.Host+":"+dst)
+	return runPassthrough(cmd)
 }
 
 // RsyncFrom runs rsync to copy from the given remote source to the given local
 // destination.
-func (t *Target) RsyncFrom(src, dst string) error {
-	c := exec.Command("rsync", "-avP", "-e", t.sshInvocation(), t.Hostname+":"+src, dst)
-	return runExecPassthrough(c)
+func (c *Client) RsyncFrom(src, dst string) error {
+	cmd := exec.Command(
+		"rsync",
+		"-avP",
+		"-e", c.sshInvocation(),
+		c.opts.Host+":"+src,
+		dst)
+	return runPassthrough(cmd)
 }
 
-func (t *Target) sshOpts() []string {
+func (c *Client) sshArgs() []string {
 	args := []string{
 		"-o", "PasswordAuthentication=no",
 		"-o", "StrictHostKeyChecking=yes",
-		"-o", "User=" + t.Username,
+		"-o", "User=" + c.opts.User,
 	}
 
-	if t.KnownHosts != nil {
-		args = append(args, "-o", "UserKnownHostsFile="+t.KnownHosts.Filename)
+	if c.opts.KnownHosts != nil {
+		args = append(args, "-o", "UserKnownHostsFile="+c.opts.KnownHosts.Filename)
 	}
 
 	return args
 }
 
-func (t *Target) sshInvocation() string {
-	return "ssh " + ShellEscapeJoin(t.sshOpts())
+func (c *Client) sshInvocation() string {
+	return "ssh " + ShellEscapeJoin(c.sshArgs())
 }
 
-func runExecPassthrough(c *exec.Cmd) error {
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	return c.Run()
+func runPassthrough(cmd *exec.Cmd) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
