3a416eb5c03430c263fb6f81eaf31da99f79c47f
Chris Kastorff <encryptio@gmail.com>
2016-04-05 22:42:27 +0000

    horizon docker image now strips unneeded environment variables
    Fixes #66

--- kube-config/docker/horizon/run.sh
+++ kube-config/docker/horizon/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -eu
 
+# Strip all environment variables that aren't whitelisted
+unset $(env | cut -d= -f1 | grep -Ev '^(HOME|PATH|HOSTNAME|HZ_.*)$')
+
 exec su -s /bin/sh horizon -c 'exec hz serve'
