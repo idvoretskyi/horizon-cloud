d8e7744cb96cda2fcd4afafa00cb0dda37ae2590
Michael Lucy <mlucy@rethinkdb.com>
2016-05-13 01:38:14 +0000

    Added http -> https redirect.

--- kube-config/docker/hzc-varnish/default.vcl.sh
+++ kube-config/docker/hzc-varnish/default.vcl.sh
@@ -16,11 +16,25 @@ sub vcl_recv {
         # Health check for Google load balancer
         return (synth(200, "OK"));
     }
+
+    if (req.http.X-Forwarded-Proto == "http") {
+        set req.http.x-redir = "https://" + req.http.host + req.url;
+        return (synth(850, "Moved permanently"));
+    }
+
     if (req.url ~ "/\$") {
         set req.url = req.url + "index.html";
     }
     set req.url = "/$BUCKET/domains/" + req.http.host + req.url;
     set req.http.host = "storage.googleapis.com";
     return (hash);
+}
+
+sub vcl_synth {
+    if (resp.status == 850) {
+        set resp.http.Location = req.http.x-redir;
+        set resp.status = 302;
+        return (deliver);
+    }
 }
 EOF
