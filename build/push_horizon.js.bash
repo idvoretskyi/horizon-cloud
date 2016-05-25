cb98d2b0ac8ef08ed7c51d76a31f8b5b8c627c7a
Michael Lucy <mlucy@rethinkdb.com>
2016-05-25 23:21:10 +0000

    update push_horizon.js

--- build/push_horizon.js.bash
+++ build/push_horizon.js.bash
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 TMPDIR=$(mktemp -d)
 trap 'rm -rf $TMPDIR' EXIT
 
-../templates/horizon.js.sh > $TMPDIR/horizon.js
-gsutil -h "Cache-Control:private,max-age=5" cp -a public-read $TMPDIR/horizon.js \
+gsutil -h "Cache-Control:private,max-age=5" cp -a public-read \
+    ../kube-config/docker/horizon/horizon/client/dist/horizon.js \
     "gs://`cat /secrets/names/storage-bucket`/horizon/horizon.js"
