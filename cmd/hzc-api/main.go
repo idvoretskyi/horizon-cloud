a5d442f4aa7dcbf576709d174b6eb5cabbdf090e
Chris Kastorff <encryptio@gmail.com>
2016-05-31 20:49:57 +0000

    cmd/hzc-api: use viper/cobra

--- cmd/hzc-api/main.go
+++ cmd/hzc-api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -11,6 +10,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/compute/v1"
 	"google.golang.org/cloud/storage"
@@ -418,131 +419,151 @@ func updateProjectManifest(
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile)
-
-	logger, err := hzlog.MainLogger("hzc-api")
-	if err != nil {
-		log.Fatal(err)
+	if err := RootCmd.Execute(); err != nil {
+		log.Println(err)
+		os.Exit(-1)
 	}
+}
 
-	log.SetOutput(hzlog.WriterLogger(logger))
+var RootCmd = &cobra.Command{
+	Use:   "hzc-client",
+	Short: "Horizon Cloud Client",
+	Long:  `A client for accessing Horizon Cloud.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.SetFlags(log.Lshortfile)
 
-	baseCtx := hzhttp.NewContext(logger)
+		logger, err := hzlog.MainLogger("hzc-api")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	listenAddr := flag.String("listen", ":8000", "HTTP listening address")
-	sharedSecretFile := flag.String(
-		"shared_secret",
+		log.SetOutput(hzlog.WriterLogger(logger))
+
+		baseCtx := hzhttp.NewContext(logger)
+
+		data, err := ioutil.ReadFile(viper.GetString("shared_secret"))
+		if err != nil {
+			log.Fatal("Unable to read shared secret file: ", err)
+		}
+		if len(data) < 16 {
+			log.Fatal("Shared secret was not long enough")
+		}
+		sharedSecret := string(data)
+
+		tokenSecret, err = ioutil.ReadFile(viper.GetString("token_secret"))
+		if err != nil {
+			log.Fatal("Unable to read token secret file: ", err)
+		}
+		if len(tokenSecret) < 16 {
+			log.Fatal("Token secret was not long enough")
+		}
+
+		rdbConn, err := db.New()
+		if err != nil {
+			log.Fatal("Unable to connect to RethinkDB: ", err)
+		}
+		baseCtx = baseCtx.WithDBConnection(rdbConn)
+
+		serviceAccountData, err := ioutil.ReadFile(viper.GetString("service_account"))
+		if err != nil {
+			log.Fatal("Unable to read service account file: ", err)
+		}
+		serviceAccount, err := google.JWTConfigFromJSON(serviceAccountData, storage.ScopeFullControl, compute.ComputeScope)
+		if err != nil {
+			log.Fatal("Unable to parse service account: ", err)
+		}
+		baseCtx = baseCtx.WithServiceAccount(serviceAccount)
+
+		region := "us-central1-f"
+		gc, err := gcloud.New(serviceAccount, clusterName, region)
+		if err != nil {
+			log.Fatal("Unable to create gcloud client: ", err)
+		}
+
+		k := kube.New(templatePath, gc)
+		baseCtx = baseCtx.WithKube(k)
+
+		go projectSync(baseCtx)
+
+		paths := []struct {
+			Path          string
+			Func          func(ctx *hzhttp.Context, w http.ResponseWriter, r *http.Request)
+			RequireSecret bool
+		}{
+			// Client uses these.
+			{api.UpdateProjectManifestPath, updateProjectManifest, false},
+
+			// Web interface uses these.
+			{api.GetProjectPath, getProject, true},
+			{api.DeleteProjectPath, deleteProject, true},
+			{api.SetProjectKubeConfigPath, setProjectKubeConfig, true},
+			{api.AddProjectUsersPath, addProjectUsers, true},
+			{api.UserCreatePath, userCreate, true},
+			{api.UserGetPath, userGet, true},
+			{api.UserAddKeysPath, userAddKeys, true},
+			{api.UserDelKeysPath, userDelKeys, true},
+			{api.SetDomainPath, setDomain, true},
+			{api.GetDomainsByProjectPath, getDomainsByProject, true},
+
+			// Other server stuff uses these.
+			{api.GetUsersByKeyPath, getUsersByKey, true},
+			{api.GetProjectAddrsByKeyPath, getProjectAddrsByKey, true},
+			{api.GetProjectAddrByDomainPath, getProjectAddrByDomain, true},
+		}
+
+		mux := hzhttp.NewMuxer()
+		for _, path := range paths {
+			var h hzhttp.Handler = hzhttp.HandlerFunc(path.Func)
+			if path.RequireSecret {
+				h = api.RequireSecret(sharedSecret, h)
+			}
+			mux.RegisterPath(path.Path, h)
+		}
+		logMux := hzhttp.LogHTTPRequests(mux)
+
+		logger.Info("Started.")
+		listenAddr := viper.GetString("listen")
+		err = http.ListenAndServe(listenAddr, hzhttp.BaseContext(baseCtx, logMux))
+		if err != nil {
+			logger.Error("Couldn't serve on %v: %v", listenAddr, err)
+		}
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	pf := RootCmd.PersistentFlags()
+
+	pf.String("listen", ":8000", "HTTP listening address")
+
+	pf.String("shared_secret",
 		"/secrets/api-shared-secret/api-shared-secret",
-		"Location of API shared secret",
-	)
+		"Location of API shared secret")
 
-	tokenSecretFile := flag.String(
-		"token_secret",
+	pf.String("token_secret",
 		"/secrets/token-secret/token-secret",
-		"Location of token secret file",
-	)
+		"Location of token secret file")
 
-	flag.StringVar(&clusterName, "cluster_name", "horizon-cloud-1239",
+	pf.StringVar(&clusterName, "cluster_name", "horizon-cloud-1239",
 		"Name of the GCE cluster to use.")
 
-	flag.StringVar(&templatePath, "template_path",
+	pf.StringVar(&templatePath, "template_path",
 		os.Getenv("HOME")+"/go/src/github.com/rethinkdb/horizon-cloud/templates/",
 		"Path to the templates to use when creating Kube objects.")
 
-	flag.StringVar(&storageBucket, "storage_bucket",
+	pf.StringVar(&storageBucket, "storage_bucket",
 		"hzc-dev-io-userdata",
 		"Storage bucket to write user objects to")
 
-	serviceAccountFile := flag.String(
-		"service_account",
+	pf.String("service_account",
 		"/secrets/gcloud-service-account/gcloud-service-account.json",
 		"Path to the JSON service account.")
 
-	flag.Parse()
+	viper.BindPFlags(pf)
+}
 
-	data, err := ioutil.ReadFile(*sharedSecretFile)
-	if err != nil {
-		log.Fatal("Unable to read shared secret file: ", err)
-	}
-	if len(data) < 16 {
-		log.Fatal("Shared secret was not long enough")
-	}
-	sharedSecret := string(data)
-
-	tokenSecret, err = ioutil.ReadFile(*tokenSecretFile)
-	if err != nil {
-		log.Fatal("Unable to read token secret file: ", err)
-	}
-	if len(tokenSecret) < 16 {
-		log.Fatal("Token secret was not long enough")
-	}
-
-	rdbConn, err := db.New()
-	if err != nil {
-		log.Fatal("Unable to connect to RethinkDB: ", err)
-	}
-	baseCtx = baseCtx.WithDBConnection(rdbConn)
-
-	serviceAccountData, err := ioutil.ReadFile(*serviceAccountFile)
-	if err != nil {
-		log.Fatal("Unable to read service account file: ", err)
-	}
-	serviceAccount, err := google.JWTConfigFromJSON(serviceAccountData, storage.ScopeFullControl, compute.ComputeScope)
-	if err != nil {
-		log.Fatal("Unable to parse service account: ", err)
-	}
-	baseCtx = baseCtx.WithServiceAccount(serviceAccount)
-
-	region := "us-central1-f"
-	gc, err := gcloud.New(serviceAccount, clusterName, region)
-	if err != nil {
-		log.Fatal("Unable to create gcloud client: ", err)
-	}
-
-	k := kube.New(templatePath, gc)
-	baseCtx = baseCtx.WithKube(k)
-
-	go projectSync(baseCtx)
-
-	paths := []struct {
-		Path          string
-		Func          func(ctx *hzhttp.Context, w http.ResponseWriter, r *http.Request)
-		RequireSecret bool
-	}{
-		// Client uses these.
-		{api.UpdateProjectManifestPath, updateProjectManifest, false},
-
-		// Web interface uses these.
-		{api.GetProjectPath, getProject, true},
-		{api.DeleteProjectPath, deleteProject, true},
-		{api.SetProjectKubeConfigPath, setProjectKubeConfig, true},
-		{api.AddProjectUsersPath, addProjectUsers, true},
-		{api.UserCreatePath, userCreate, true},
-		{api.UserGetPath, userGet, true},
-		{api.UserAddKeysPath, userAddKeys, true},
-		{api.UserDelKeysPath, userDelKeys, true},
-		{api.SetDomainPath, setDomain, true},
-		{api.GetDomainsByProjectPath, getDomainsByProject, true},
-
-		// Other server stuff uses these.
-		{api.GetUsersByKeyPath, getUsersByKey, true},
-		{api.GetProjectAddrsByKeyPath, getProjectAddrsByKey, true},
-		{api.GetProjectAddrByDomainPath, getProjectAddrByDomain, true},
-	}
-
-	mux := hzhttp.NewMuxer()
-	for _, path := range paths {
-		var h hzhttp.Handler = hzhttp.HandlerFunc(path.Func)
-		if path.RequireSecret {
-			h = api.RequireSecret(sharedSecret, h)
-		}
-		mux.RegisterPath(path.Path, h)
-	}
-	logMux := hzhttp.LogHTTPRequests(mux)
-
-	logger.Info("Started.")
-	err = http.ListenAndServe(*listenAddr, hzhttp.BaseContext(baseCtx, logMux))
-	if err != nil {
-		logger.Error("Couldn't serve on %v: %v", *listenAddr, err)
-	}
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetEnvPrefix("hzc")
+	viper.AutomaticEnv() // read in environment variables that match
 }
