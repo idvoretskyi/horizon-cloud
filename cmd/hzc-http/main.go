1406e1aea9184be950e0789607cb6a82a70f080d
Chris Kastorff <encryptio@gmail.com>
2016-05-31 21:12:26 +0000

    cmd/hzc-http: remove api secret and tls support

--- cmd/hzc-http/main.go
+++ cmd/hzc-http/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -32,13 +31,7 @@ func init() {
 	pf := RootCmd.PersistentFlags()
 
 	pf.StringP("listen", "l", ":80", "Address to listen for HTTP connections on.")
-	pf.StringP("listen_tls", "", "", "Address to listen for HTTPS connections on.")
-	pf.StringP("tls_cert", "", "", "Path to TLS certificate chain")
-	pf.StringP("tls_key", "", "", "Path to TLS key")
 	pf.StringP("api_server", "a", "http://api-server:8000", "API server base URL.")
-	pf.StringP("secret_path", "s",
-		"/secrets/api-shared-secret/api-shared-secret",
-		"Path to API server shared secret")
 
 	viper.BindPFlags(pf)
 }
@@ -67,13 +60,7 @@ var RootCmd = &cobra.Command{
 
 		baseCtx := hzhttp.NewContext(logger)
 
-		secretPath := viper.GetString("secret_path")
-		secret, err := ioutil.ReadFile(secretPath)
-		if err != nil {
-			log.Fatalf("Couldn't read api server secret from %v: %v", secretPath, err)
-		}
-
-		conf.APIClient, err = api.NewClient(viper.GetString("api_server"), string(secret))
+		conf.APIClient, err = api.NewClient(viper.GetString("api_server"), "")
 		if err != nil {
 			log.Fatalf("Couldn't create API client: %v", err)
 		}
@@ -82,14 +69,6 @@ var RootCmd = &cobra.Command{
 		handler = hzhttp.LogHTTPRequests(handler)
 
 		plainHandler := hzhttp.BaseContext(baseCtx, handler)
-
-		if tlsAddr := viper.GetString("listen_tls"); tlsAddr != "" {
-			go func() {
-				certFile := viper.GetString("tls_cert")
-				keyFile := viper.GetString("tls_key")
-				log.Fatal(http.ListenAndServeTLS(tlsAddr, certFile, keyFile, plainHandler))
-			}()
-		}
 
 		log.Fatal(http.ListenAndServe(viper.GetString("listen"), plainHandler))
 	},
