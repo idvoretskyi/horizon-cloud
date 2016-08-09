cdf8f204c1cbe68c64f13f41fd3b8b17bb8b410e
Chris Kastorff <encryptio@gmail.com>
2016-08-09 23:56:11 +0000

    internal/api: add first 512 bytes of response body in json deserialize failure message

--- internal/api/util.go
+++ internal/api/util.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -37,10 +39,17 @@ func WriteJSONResp(rw http.ResponseWriter, code int, i interface{}) {
 }
 
 func ReadJSONResp(hr *http.Response, i interface{}) error {
-	var resp Resp
-	err := json.NewDecoder(hr.Body).Decode(&resp)
-	if err != nil {
+	firstPart := make([]byte, 512)
+	n, err := hr.Body.Read(firstPart)
+	if n == 0 && err != nil {
 		return err
+	}
+	firstPart = firstPart[:n]
+
+	var resp Resp
+	err = json.NewDecoder(io.MultiReader(bytes.NewReader(firstPart), hr.Body)).Decode(&resp)
+	if err != nil {
+		return fmt.Errorf("Couldn't deserialize JSON: %v on body %#v...", err, string(firstPart))
 	}
 	if !resp.Success || hr.StatusCode != http.StatusOK {
 		return fmt.Errorf("server error (%s): %s", hr.Status, resp.Error)
